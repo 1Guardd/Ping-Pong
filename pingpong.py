f39f283487db4a077d6562bd5277825e9ff7e46c
1Guardd <elsufevartem@gmail.com>
2026-05-17 10:55:11 +0300

    рабочий игровой цикл

--- pingpong.py
+++ pingpong.py
@@ -14,10 +14,10 @@ class GameSprite(sprite.Sprite):
         self.speed = player_speed
         self.rect =Rect(player_x,player_y,player_l,player_w)
     def reset(self):
-        draw(wind,(0,125,225),self.rect)
+        draw.rect(wind,(0,125,225),self.rect)
 class Player(GameSprite):
     def __init__(self,player_x,player_y,player_speed,KUP,KDOWN,player_l=15,player_w=65):
-        super().__init__(self,player_x,player_y,player_speed,player_l,player_w)
+        super().__init__(player_x,player_y,player_speed,player_l,player_w)
         self.KUP = KUP
         self.KDOWN = KDOWN
     def move(self):
@@ -26,8 +26,18 @@ class Player(GameSprite):
             self.rect.y -=self.speed
         if keys_press[self.KDOWN] and self.rect.y<w_w-80:
             self.rect.y +=self.speed
-racket1 = Player(50,0,5,K_w,K_s)
-racket2 = Player(w_l-50,0,K_UP,K_DOWN)
+racket1 = Player(50,0,1,K_w,K_s)
+racket2 = Player(w_l-50,0,1,K_UP,K_DOWN)
+while game:
+    wind.fill((0,0,255))
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    racket1.move()
+    racket2.move()
+    racket1.reset()
+    racket2.reset()
+    display.update()
 
 
     
